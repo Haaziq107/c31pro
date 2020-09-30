349e9178e436760b551c8211fec4bc963c7e9bbc
Haaziq107 <66906567+Haaziq107@users.noreply.github.com>
2020-09-30 19:02:29 +0530

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,115 @@
+class Game{
+    constructor(){
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+    player1.addImage("player1",player_img);
+    
+    player2 = createSprite(800,500);
+    player2.addImage("player2", player_img);
+    players=[player1,player2];
+
+        }
+    
+    play(){
+        
+                form.hide();
+
+                Player.getPlayerInfo();
+                 image(back_img, 0, 0, 1000, 800);
+                 var x =100;
+                 var y=200;
+                 var index =0;
+                 drawSprites();
+                 for(var plr in allPlayers){
+                    
+                    players[index -1].X = X;
+                     players[index - 1].Y = Y;
+
+                     index = index+1;
+                     x = 500-allPlayers[plr].distance;
+                     y=500;
+                     
+                     
+                       
+                     if(index === player.index){
+                         
+                         fill("black");
+                         textSize(25);
+                         text(allPlayers[plr].name ,x-25,y+25);
+
+                         
+                     }
+                
+                 }
+
+                if (keyIsDown(RIGHT_ARROW) && player.index !== null) {
+                    player.velocityX = -10
+                    player.update();
+                }
+                
+                if (keyIsDown(LEFT_ARROW) && player.index !== null) {
+                    player.velocityX = 10
+                    player.update();
+                }
+            
+                 if (frameCount % 20 === 0) {
+                     fruits = createSprite(random(100, 1000), 0, 100, 100);
+                     fruits.velocityY = 6;
+                     var rand = Math.round(random(1,5));
+                     switch(rand){
+                         case 1: fruits.addImage(fruit1_img);
+                         break;
+                         case 2: fruits.addImage(fruit2_img);
+                         break;
+                         case 3: fruits.addImage(fruit3_img);
+                         break;
+                         case 4: fruits.addImage(fruit4_img);
+                         break;
+                         case 5: fruits.addImage(fruit5_img);
+                         break;
+                     }
+                     fruitGroup.add(fruits);
+                     
+                 }
+                 
+                  if (player.index !== null) {
+                    // fill code here, to destroy the objects.
+                    if(fruits.isTouching(player1)){
+                        fruits.distroy();
+                    }
+                    if(fruits.isTouching(player2)){
+                       fruits.distroy();
+                   }
+                  }
+
+    }
+
+    end(){
+       console.log("Game Ended");
+    }
+}
